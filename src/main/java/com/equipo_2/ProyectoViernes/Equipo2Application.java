0ecd6835f6359d14f1969a864a3cde558aba89eb
Kevin Chan <kevin.chan1512@gmail.com>
2025-01-14 18:09:50 -0600

    Comienzo del proyecto

---
+++ src/main/java/com/equipo_2/ProyectoViernes/Equipo2Application.java
@@ -0,0 +1,13 @@
+package com.equipo_2.ProyectoViernes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Equipo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Equipo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipo_2/ProyectoViernes/Equipo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo_2.ProyectoViernes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Equipo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
